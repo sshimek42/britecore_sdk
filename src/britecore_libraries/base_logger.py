e39bc3c8af6c50c63954c9d0ba290164ea4d90c9
sshimek42 <shaun.shimek@shimekit.com>
2025-12-11 14:17:20 -0600

    Workflow updates

---
+++ src/britecore_libraries/base_logger.py
@@ -0,0 +1,24 @@
+from sclogging import sclogging_main as scl
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(
+                *args, **kwargs
+                )
+        return cls._instances[cls]
+
+
+class SCLogger(metaclass=Singleton):
+
+    _logger = None
+
+    def __init__(self, *args, **kwargs):
+        if not self._logger:
+            self._logger = scl.get_logger(*args, **kwargs)
+
+    def get_logger(self):
+        return self._logger
